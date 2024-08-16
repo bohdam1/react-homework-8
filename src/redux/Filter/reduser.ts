36875bc11319e1a50b3124d2dd0236b5b7ef0986
bohdam1 <mochuradb@gmail.com>
2024-08-16 09:58:30 +0300

    fix-deploy

--- src/redux/Filter/reduser.ts
+++
@@ -1,8 +0,0 @@
-// import { createReducer } from "@reduxjs/toolkit";
-// import { filterInitState } from "./init-state";
-// import { filterAction } from "./ection";
-
-
-// export const filterReducer = createReducer(filterInitState, (builder)=>{
-//     builder.addCase(filterAction, (state, {payload}) => {state.filter = payload})
-// });
